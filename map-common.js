15d2c088d3e59f1242b33781f0e90e8d8eb4c8cd
Ajmal Vh <ajmalcpp@gmail.com>
2013-06-06 17:57:37 +0530

    Initial Commit

---
+++ map-common.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function(){
+    $('.map').each(function () {
+        var location = $(this).attr("data-location");
+        var divId = $(this).attr("id");
+        var marker = $(this).attr("data-marker");
+        if (divId && location) {
+            ShowMap(location.split(","), divId,marker);
+        }
+    }); 
+});
+function ShowMap(location, pH, marker) {
+    if (location.length < 3) return;
+    var startLat = location[0];
+    var startLon = location[1];
+    var zoom = parseInt( location[2]);
+    var image = marker;
+    var stylez = [{ featureType: "all", elementType: "all", stylers: [{ saturation: -95 }] }];
+    var mapOptions = { zoom: zoom, mapTypeId: google.maps.MapTypeId.ROADMAP, center: new google.maps.LatLng(startLat, startLon) };
+    var map = new google.maps.Map(document.getElementById(pH), mapOptions);
+    var marker = new google.maps.Marker({ position: new google.maps.LatLng(startLat, startLon), map: map, title: '', icon: image });
+}
